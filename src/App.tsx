4517436e76102224eafee3b0c6c578884eeaa0cc
Usman22209 <166890135+Usman22209@users.noreply.github.com>
2025-02-01 18:26:32 +0500

    paths set

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import Page from "@pages/Page";
+const App = () => {
+  return <div className="bg-red-500 text-blue-500"><Page/></div>;
+};
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "src/components"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@routes": path.resolve(__dirname, "src/routes"),
+      "@layouts": path.resolve(__dirname, "src/layouts"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+    },
+  },
+});
